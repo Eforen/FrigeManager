c7747681047afa2c7df134b6265fe0ce865cce1c
Ariel Lothlorien <gitkracken@arielkauai.com>
2020-04-10 17:12:24 -0500

    Fix Docker Spinup

---
+++ setupNewEnv.sh
@@ -0,0 +1,8 @@
+docker-compose up -d --build
+
+docker-compose exec web cp -n /var/www/.env.example /var/www/.env
+
+docker-compose exec web bash /var/www/fixPerms.sh
+
+docker-compose exec web php artisan key:generate
+docker-compose exec web php artisan config:cache
